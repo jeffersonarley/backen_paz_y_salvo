dd325f36756ea457b338b391fb63ed4dd291678d
Paula Rache <paularache04@gmail.com>
2026-08-01 11:37:20 -0500

    chore: ignorar carpeta temporal .quasar

--- .quasar/pinia.d.ts
+++
@@ -1,9 +0,0 @@
-/* oxlint-disable */
-/* eslint-disable */
-import { Router } from 'vue-router';
-
-declare module 'pinia' {
-  export interface PiniaCustomProperties {
-    readonly router: Router;
-  }
-}
