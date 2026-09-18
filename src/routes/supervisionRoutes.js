21872f8985a2eedcc506a35f46ddb6c69628d0ba
digmon318 <diegomonsalvetavera@gmail.com>
2026-09-17 22:18:42 -0500

    Delete src directory

--- src/routes/supervisionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const supervisionController = require('../controllers/supervisionController');
-const { verificarToken, verificarRol } = require('../middlewares/authMiddleware');
-
-// Endpoint del Diagrama 3 (exclusivo Supervisor)
-router.put('/evaluar/:id', verificarToken, verificarRol('Supervisor'), supervisionController.evaluarContrato);
-
-module.exports = router;
--- src/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Envuelve un controlador async para propagar errores al errorHandler central
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
